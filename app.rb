b26791d0317369c34a2e18def388cee0ba5c51b6
kevin <kevinacho_z900@hotmail.com>
2017-11-11 00:56:53 -0400

    Lanzamiento de flecha a una caverna disponible

--- app.rb
+++ app.rb
@@ -1,23 +1,28 @@
 require 'sinatra'
 require './lib/map'
+require './lib/cavern'
+require './lib/caverns'
+require './lib/player'
 
 $message
 $comando
 
 
-partida = Map.new()
+$partida
 
 get '/' do
   erb :menu 
 end
 
 get '/gameOption' do
-  erb :opcionJuego 
+	$partida = Map.new() 
+	$messageError="" 
+	erb :opcionJuego
 end
 
 get '/game' do
-  $posPlayer = "Estas en la caverna " + partida.reportPosicionPlayer.to_s
-  $messageCaverns = "Tienes acceso a las cavernas del: " + partida.reportPosicionesAMoverse
+  $posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+  $messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
   $message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
   erb :juego
 end
@@ -26,63 +31,80 @@ post '/game' do
   comando= params[:instruccion].to_s
 
   if (comando == 'M' || comando == 'm')
-
-  	
-	$posPlayer = "Estas en la caverna " + partida.reportPosicionPlayer.to_s
-	$messageCaverns = "Tienes acceso a las cavernas del: " + partida.reportPosicionesAMoverse
-	$message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
-  	$mensaje = "Indique a que punto cardinal quiere ir: "
+		$messageError="" 
+		$posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+		$messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
+		$message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
+	 	$mensaje = "Indique a que punto cardinal quiere ir: "
   	erb :movida
     
-  else
-  	$messageError = "Letra incorrecta, vuelva a ingresar su opción."
-  	erb :juego
-  end
+	elsif (comando == 'F' || comando == 'f')
+		if($partida.getPlayer.getArrows.to_i<=0)
+			$messageError = "No tiene suficientes flechas."
+			erb :juego
+		else
+			$messageError=""
+			$posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+			$messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
+			$message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
+			 $mensaje = "Indique a que punto cardinal quiere lanzar la flecha: "
+			erb :disparo
+		end
+	else
+			$messageError = "Letra incorrecta, vuelva a ingresar su opción."
+			erb :juego
+	end
+	
+	
   
 end
 
-
+#movimientos
 post '/movida' do
   comando= params[:instruccion].to_s
 
-  if (comando == 'N' || comando == 'n')
-  	if (partida.hasAccessToNorth)
-  		  partida.moveToNorth
-  		  $posPlayer = "Estas en la caverna " + partida.reportPosicionPlayer.to_s
-		  $messageCaverns = "Tienes acceso a las cavernas del: " + partida.reportPosicionesAMoverse
+	if (comando == 'N' || comando == 'n')
+		$messageError=""
+  	if ($partida.hasAccessToNorth)
+  		  $partida.moveToNorth
+  		  $posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+		  $messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
 		  $message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
 		  erb :juego
   	else
   		$messageError = "No se puede ir a esa dirección."
   		erb :movida
   	end
-  elsif (comando == 'S' || comando == 's')
-  	if (partida.hasAccessToSouth)
-  		  partida.moveToSouth
-  		  $posPlayer = "Estas en la caverna " + partida.reportPosicionPlayer.to_s
-		  $messageCaverns = "Tienes acceso a las cavernas del: " + partida.reportPosicionesAMoverse
+	elsif (comando == 'S' || comando == 's')
+		$messageError=""
+  	if ($partida.hasAccessToSouth)
+  		  $partida.moveToSouth
+  		  $posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+		  $messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
 		  $message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
 		  erb :juego
   	else
   		$messageError = "No se puede ir a esa dirección."
   		erb :movida
   	end
-  elsif (comando == 'E' || comando == 'e')
-  	if (partida.hasAccessToEast)
-  		  partida.moveToEast
-  		  $posPlayer = "Estas en la caverna " + partida.reportPosicionPlayer.to_s
-		  $messageCaverns = "Tienes acceso a las cavernas del: " + partida.reportPosicionesAMoverse
+	elsif (comando == 'E' || comando == 'e')
+		$messageError=""
+  	if ($partida.hasAccessToEast)
+  		  $partida.moveToEast
+  		  $posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+		  $messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
 		  $message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
 		  erb :juego
   	else
   		$messageError = "No se puede ir a esa dirección."
   		erb :movida
   	end
-  elsif (comando == 'O' || comando == 'o')
-  	if (partida.hasAccessToWest)
-  		  partida.moveToWest
-  		  $posPlayer = "Estas en la caverna " + partida.reportPosicionPlayer.to_s
-		  $messageCaverns = "Tienes acceso a las cavernas del: " + partida.reportPosicionesAMoverse
+	elsif (comando == 'O' || comando == 'o')
+		$messageError=""
+  	if ($partida.hasAccessToWest)
+  		  $partida.moveToWest
+  		  $posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+		  $messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
 		  $message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
 		  erb :juego
   	else
@@ -92,6 +114,91 @@ post '/movida' do
   else
   	$messageError = "No se puede ir a esa dirección."
   	erb :movida
+  end
+  
+end
+
+#disparos
+post '/disparo' do
+	comando= params[:instruccion].to_s
+	@player
+	@arrowsPlayer
+	@arrows
+	if (comando == 'N' || comando == 'n')
+		$messageError=""
+		if ($partida.hasAccessToNorth)
+			@player=$partida.getPlayer
+			@arrowsPlayer=@player.getArrows
+			 @player.setArrows=@arrowsPlayer.to_i - 1
+			$partida.setPlayer=@player
+			@arrows = @player.getArrows
+		
+  		  $posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+		  $messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
+		  $message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
+			$messageError = "La flecha falló, el número de flechas restantes es: "+@arrows.to_s
+			erb :juego
+		else
+  		$messageError = "No se puede lanzar a esa dirección."+@arrows.to_s
+  		erb :disparo
+  	end
+	elsif (comando == 'S' || comando == 's')
+		$messageError=""
+		if ($partida.hasAccessToSouth)
+			@player=$partida.getPlayer
+			@arrowsPlayer=@player.getArrows
+			 @player.setArrows=@arrowsPlayer.to_i - 1
+			$partida.setPlayer=@player
+			@arrows = @player.getArrows
+			
+  		  $posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+		  $messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
+		  $message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
+			$messageError = "La flecha falló, el número de flechas restantes es: "+@arrows.to_s
+			erb :juego
+  	else
+  		$messageError = "No se puede lanzar a esa dirección."+@arrows.to_s
+  		erb :disparo
+  	end
+	elsif (comando == 'E' || comando == 'e')
+		$messageError=""
+		if ($partida.hasAccessToEast)
+			@player=$partida.getPlayer
+			@arrowsPlayer=@player.getArrows
+			 @player.setArrows=@arrowsPlayer.to_i - 1
+			$partida.setPlayer=@player
+			@arrows = @player.getArrows
+
+  		  $posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+		  $messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
+		  $message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
+			$messageError = "La flecha falló, el número de flechas restantes es: "+@arrows.to_s
+			erb :juego
+  	else
+  		$messageError = "No se puede lanzar a esa dirección."+@arrows.to_s
+  		erb :disparo
+  	end
+	elsif (comando == 'O' || comando == 'o')
+		$messageError=""
+		if ($partida.hasAccessToWest)
+			@player=$partida.getPlayer
+			@arrowsPlayer=@player.getArrows
+			 @player.setArrows=@arrowsPlayer.to_i - 1
+			$partida.setPlayer=@player
+			@arrows = @player.getArrows
+
+  		  $posPlayer = "Estas en la caverna " + $partida.reportPosicionPlayer.to_s
+		  $messageCaverns = "Tienes acceso a las cavernas del: " + $partida.reportPosicionesAMoverse
+		  $message = "Elige una jugada: moverse 'M', lanzar flecha 'F' o usar sprite 'S' y presione Enter."
+			$messageError = "La flecha falló, el número de flechas restantes es: "+@arrows.to_s
+			erb :juego
+  	else
+  		$messageError = "No se puede lanzar a esa dirección."+@arrows.to_s
+  		erb :disparo
+  	end
+  else
+  	$messageError = "No se puede lanzar a esa dirección."+@arrows.to_s
+  	erb :disparo
   end
   
 end
